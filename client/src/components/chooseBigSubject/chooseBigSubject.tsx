9fdc1db53941ef7a6d8e14a6a7a9c3615fe7c781
eyal porat <eyal1.porat@gmail.com>
2024-03-16 21:31:56 +0200

    ui changes

--- client/src/components/chooseBigSubject/chooseBigSubject.tsx
+++ client/src/components/chooseBigSubject/chooseBigSubject.tsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import styles from './chooseBigSubject.module.css';
 import { useNavigate } from 'react-router-dom';
 import { useContentContext } from '../../context/ContentContext';
+import { FaArrowRight } from "react-icons/fa6";
+import { FaWandMagicSparkles } from "react-icons/fa6";
+import { FcBinoculars } from "react-icons/fc";
 
 const ChooseBigSubject: React.FC<any> = (props) => {
     const { updateBigSubject } = useContentContext();
@@ -20,25 +23,33 @@ const ChooseBigSubject: React.FC<any> = (props) => {
     };
 
     return (
-        <div className={styles.allPage}>
-            <label className={styles.label}>choose one subject</label>
             <div className={styles.container}>
+                
+                <div className={styles.label}>
+                    <label>Choose Subject</label>
+                    <FcBinoculars></FcBinoculars>
+                </div>
+
                 {props.subjects.map((subject: any, index: number) => (
                     <button
                         key={index}
-                        style={selectedSubjectIndex === index ? { border: '2px solid red' } : {}}
+                        className={styles.btn}
+                        style={selectedSubjectIndex === index ? { border: '2px solid orange' } : {}}
                         onClick={() => handleClick(index, subject)}
                     >
-                        {subject}
+                        <label>{subject}</label>
+                        <FaArrowRight className={styles.icon}></FaArrowRight>
                     </button>
                 ))}
+
+                {selectedSubjectIndex !== null && (
+                    <button className={styles.btn_navigate} onClick={handleNavigate}>
+                        <FaWandMagicSparkles></FaWandMagicSparkles>
+                        <label>Go to the next step</label>
+                    </button>
+                )}
+
             </div>
-            {selectedSubjectIndex !== null && ( // Conditional rendering based on selectedSubjectIndex
-                <button className={styles.btn_navigate} onClick={handleNavigate}>
-                    Go On
-                </button>
-            )}
-        </div>
     );
 };
 
